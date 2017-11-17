4d7e8aa68b6ec228a5aeb3505265f66896ec609c
Stepan Snigirev <ss@Stepans-MacBook-Pro.local>
2017-11-17 12:25:07 +0100

    use window.crypto web API instead of CryptoJS

--- emoji.js
+++ emoji.js
@@ -5,59 +5,70 @@ $(document).ready( () => {
     var requiredEntropy = 132;
     var prefix = "80";
 
-    function trySeed(){
+    var enc = new TextEncoder("utf-8");
+    var key;
+
+    function getHash(str){
+        return window.crypto.subtle.sign("HMAC", key, enc.encode(str)).then(signature => {
+            var b = new Uint8Array(signature);
+            var hash = Array.prototype.map.call(b, x => ('00'+x.toString(16)).slice(-2)).join("")
+            return {str: str, hash: hash};
+        });
+    }
+
+    function makeRandomSeed(){
         var str = "";
         var array = new Uint16Array(Math.ceil(requiredEntropy*Math.log(2)/Math.log(len)));
         window.crypto.getRandomValues(array);
         for (var i = 0; i < array.length; i++) {
             str += emojis[array[i] % len];
         }
-
-        // (message, key)
-        var hmac = CryptoJS.HmacSHA512(str, "Seed version").toString();
-        return {
-            str: str,
-            hmac: hmac,
-        };
+        return str;
     }
 
-    var o;
+    function display(o){
+        $("#seed").val(o.str);
 
-    o = trySeed();
-    while(!o.hmac.startsWith(prefix)){
-        o = trySeed();
+        var node = bitcoin.HDNode.fromSeedHex(o.hash);
+        var xprv = node.toBase58();
+        var xpub = node.neutered().toBase58();
+
+        $("#hmac").html(o.hash);
+        $("#xprv").html(xprv);
+        $("#xpub").html(xpub);
+
+        if(o.hash.startsWith(prefix)){
+            $("#seed").addClass("is-valid");
+            $("#seed").removeClass("is-invalid");
+        }else{
+            $("#seed").addClass("is-invalid");
+            $("#seed").removeClass("is-valid");
+        }
     }
-    $("#seed").val(o.str);
 
-    o.node = bitcoin.HDNode.fromSeedHex(o.hmac);
-    o.xprv = o.node.toBase58();
-    o.xpub = o.node.neutered().toBase58();
+    function trySeed(){
+        var str = makeRandomSeed();
+        getHash(str).then( o => {
+            if(o.hash.startsWith(prefix)){
+                display(o);
+            }else{
+                trySeed();
+            }
+        });
+    }
 
-    $("#hmac").html(o.hmac);
-    $("#xprv").html(o.xprv);
-    $("#xpub").html(o.xpub);
-
-    $("#seed").addClass("is-valid");
-    $("#seed").removeClass("is-invalid");
+    window.crypto.subtle.importKey(
+        "raw", enc.encode("Seed version"), { name: "HMAC", hash: {name: "SHA-512"} }, false, ["sign", "verify"]
+    ).then( k => {
+        key = k;
+        trySeed();
+    });
 
     $("input").on("input", e => {
-    	o.str = $("#seed").val();
-        o.hmac = CryptoJS.HmacSHA512(o.str, "Seed version").toString();
-        o.node = bitcoin.HDNode.fromSeedHex(o.hmac);
-        o.xprv = o.node.toBase58();
-        o.xpub = o.node.neutered().toBase58();
-
-    	$("#hmac").html(o.hmac);
-    	$("#xprv").html(o.xprv);
-        $("#xpub").html(o.xpub);
-        
-    	if(o.hmac.startsWith(prefix)){
-    		$("#seed").addClass("is-valid");
-    		$("#seed").removeClass("is-invalid");
-    	}else{
-    		$("#seed").addClass("is-invalid");
-    		$("#seed").removeClass("is-valid");
-    	}
+    	str = $("#seed").val();
+        getHash(str).then( o => {
+            display(o);
+        });
     });
 
 });
